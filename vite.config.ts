f04c0ec96e826e2a754aaf2c50ded36870503e38
tangopj <rail.batyrshin.js@gmail.com>
2025-05-09 02:07:59 +0600

    init configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import {resolve} from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: "./src/hooks/useStore.ts",
+      entry: resolve(__dirname, "./src/hooks/useStore.ts"),
       name: "react-global-signals",
-      fileName: "react-global-signals[hash]",
+      formats: ["es", "umd"],
+      fileName: (format) => `react-global-signals.${format}.js`,
     },
     rollupOptions: {
       external: ["react"],
