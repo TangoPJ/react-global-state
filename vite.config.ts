dfd738bdcd5c66e75627224090b0a6a41cb4bf6c
tangopj <rail.batyrshin.js@gmail.com>
2025-05-09 14:44:56 +0600

    tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,26 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [react(), dts({ insertTypesEntry: true, outDir: "dist" })],
   build: {
     lib: {
-      entry: resolve(__dirname, "./src/hooks/useStore.ts"),
-      name: "react-global-signals",
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ReactGlobalSignals",
       formats: ["es", "umd"],
-      fileName: (format) => `react-global-signals.${format}.js`,
+      fileName: (format) =>
+        `react-global-signals.${format === "es" ? "es.mjs" : "umd.js"}`,
     },
     rollupOptions: {
-      external: ["@preact/signals-react"],
+      external: ["react", "react-dom", "@preact/signals-react"],
       output: {
-        name: "react-global-signals",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@preact/signals-react": "PreactSignals",
+        },
       },
     },
     sourcemap: true,
+    minify: "esbuild",
   },
 });
